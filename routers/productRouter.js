2811ece73c9267c390d6aad7a903be57b32d4d7c
Suvadip Maiti <47469185+SuvadipMaiti@users.noreply.github.com>
2021-10-28 19:13:53 +0530

    MIDDLEWARE: authcheck

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const productController = require('../controllers/productController');
+const checkAuthMiddleware = require('../middleware/check-auth');
+
 
 const router = express.Router();
 
 router.get('/', productController.index);
 router.get('/:id', productController.show);
-router.post('/', productController.create);
-router.patch('/:id', productController.update);
-router.delete('/:id', productController.destroy);
+router.post('/', checkAuthMiddleware.checkAuth, productController.create);
+router.patch('/:id', checkAuthMiddleware.checkAuth, productController.update);
+router.delete('/:id', checkAuthMiddleware.checkAuth, productController.destroy);
 
 
 
