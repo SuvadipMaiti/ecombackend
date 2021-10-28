f68538afcb5fb5a045267fa6a5bc7bf1a4d13924
Suvadip Maiti <47469185+SuvadipMaiti@users.noreply.github.com>
2021-10-28 22:14:47 +0530

    CORS : cmd: npm install cors

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const app = express();
 const bodyParser = require('body-parser');
 //rest api data in log
 const morgan = require('morgan');
+const cors = require('cors');
 
+//https://github.com/bhaikaju/mega-back/blob/master/config/helpers.js
+//https://github.com/bhaikaju/mega-front
+//https://www.youtube.com/watch?v=38L3E-Zrswo&list=PLG3j59vX4yLHA-wCw7KDP-i0r10ZrckqG&index=2
+/* CORS */
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
 
 //prefex url in env file
 require('dotenv/config');
