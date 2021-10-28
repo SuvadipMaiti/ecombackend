fa073948267a4f8f78330b8208057f208d85ac99
Suvadip Maiti <47469185+SuvadipMaiti@users.noreply.github.com>
2021-10-28 16:22:44 +0530

    To pass json install body-perser : npm install --save body-parser

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+//json fetch
+const bodyParser = require('body-parser');
+
+
 
 //prefex url in env file
 require('dotenv/config');
@@ -7,6 +11,9 @@ const api = process.env.API_URL;
 
 const productRouter = require('./routers/productRouter');
 
-app.use(`${api}/products`,productRouter);
+
+app.use(express.json());
+
+app.use(`${api}/products`, productRouter);
 
 module.exports = app;
